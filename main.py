bd32d8a353bb843cf0cdfb1882a265b2e7116628
JeffTheK <jeff144@protonmail.com>
2021-07-30 16:06:36 +0300

    Add friends

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ name = "Shwaika"
 print("hello " + name)
 age = input("How old are you?")
 print("you are " + str(age) + " years old")
+
+# list
+friends = ["Kevin", "John", "Karen"]
+print("my friens are " + str(friends))
+print("first friend is: " + friends[0])
